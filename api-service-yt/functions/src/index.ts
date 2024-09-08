c21fd1ab77dadd8157bc4cb82a739612cb9b88de
Roka <kazakov302215@gmail.com>
2024-09-08 00:05:41 -0600

    added func for urlToken

--- api-service-yt/functions/src/index.ts
+++ api-service-yt/functions/src/index.ts
@@ -2,10 +2,15 @@ import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = getFirestore("cln-yt-firestore");
+const storage = new Storage();
+
+const rawVideoBucketName = "raw-bucket-vids";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -17,4 +22,26 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (req) => {
+  if (!req.auth) {
+    throw new functions.https.HttpsError("failed-precondition", "User must be authenticated");
+  }
+
+  const auth = req.auth;
+  const data = req.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Get uniquie filename
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
