122487e5a0b593aac958a82ccf967984e3dee759
Roka <kazakov302215@gmail.com>
2024-09-11 01:42:27 -0600

    fetch videos from storage

--- api-service-yt/functions/src/index.ts
+++ api-service-yt/functions/src/index.ts
@@ -1,47 +1,62 @@
-import * as functions from 'firebase-functions'
-import { initializeApp } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
-import * as logger from 'firebase-functions/logger'
-import { Storage } from '@google-cloud/storage'
-import { onCall } from 'firebase-functions/v2/https'
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
-initializeApp()
+initializeApp();
 
-const firestore = getFirestore('cln-yt-firestore')
-const storage = new Storage()
+const firestore = getFirestore("cln-yt-firestore");
+const storage = new Storage();
 
-const rawVideoBucketName = 'raw-bucket-vids'
+const rawVideoBucketName = "raw-bucket-vids";
+const videoCollectionId = "video";
 
-export const createUser = functions.auth.user().onCreate(user => {
+export interface Video {
+  id?: string
+  uid?: string
+  filename?: string
+  status?: "processing" | "completed"
+  title?: string
+  description?: string
+}
+
+export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
     email: user.email,
-    photoUrl: user.photoURL
-  }
+    photoUrl: user.photoURL,
+  };
 
-  firestore.collection('users').doc(user.uid).set(userInfo)
-  logger.info(`User Created: ${JSON.stringify(userInfo)}`)
-  return
-})
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
 
-export const generateUploadUrl = onCall({ maxInstances: 1 }, async req => {
+export const generateUploadUrl = onCall({maxInstances: 1}, async (req) => {
   if (!req.auth) {
-    throw new functions.https.HttpsError('failed-precondition', 'User must be authenticated')
+    throw new functions.https.HttpsError("failed-precondition", "User must be authenticated");
   }
 
-  const auth = req.auth
-  const data = req.data
-  const bucket = storage.bucket(rawVideoBucketName)
+  const auth = req.auth;
+  const data = req.data;
+  const bucket = storage.bucket(rawVideoBucketName);
 
   // Get uniquie filename
-  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
   // Get a v4 signed URL for uploading file
   const [url] = await bucket.file(fileName).getSignedUrl({
-    version: 'v4',
-    action: 'write',
-    expires: Date.now() + 15 * 60 * 1000 // 15 minutes
-  })
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
 
-  return { url, fileName }
-})
+  return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot = await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
+});
